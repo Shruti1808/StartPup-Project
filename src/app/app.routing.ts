88bb7fd67fcea7a4e25295bb0a1f075eed4d0249
Grinil Khanna and Shruti Priya <grinil+shruti@>
2017-04-10 13:26:51 -0700

    add routes for different component generated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,27 @@
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 import { HomeComponent } from './home/home.component';
+
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'users/:id',
+    component: UserDetailComponent
+  },
+  {
+    path: 'projects/:id',
+    component: ProjectDetailComponent
+  },
+  {
+    path: '',
+    component: ProjectListComponent
+  },
+
  ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes)
