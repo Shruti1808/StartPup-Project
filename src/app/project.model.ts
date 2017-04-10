c8f94e7eafe4cadc0fac2c56f410b9b8aea0410e
Alexandra Holcombe and Elise St Hilaire <alexandra+elise@>
2017-04-10 10:25:38 -0700

    Format project model

--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,2 +1,14 @@
+import ( Need ) from './need.model';
+import ( User ) from './user.model';
+
 export class Project {
+  constructor(
+    public title: string,
+    public image: string,
+    public description: string,
+    public needs: Need[],
+    public socialMedia: string[],
+    public contactInformation: string[],
+    public owner: User
+  );
 }
