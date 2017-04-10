ab2f4ec981ab233bfa08effca0e12e8aa7a4ac5d
Grinil Khanna and Shruti Priya <grinil@>
2017-04-10 10:46:58 -0700

    create user mock file with hard-coded data

---
+++ src/app/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from './user.model';
+
+export const USERS: User[]= [
+  new User("Linda wood", "Vancouver,Canada", "linda21@gmail.com", "https://ksr-ugc.imgix.net/assets/009/526/989/285cd8992aeeb3efefcd5c6e30255f7d_original.jpg?w=220&h=220&fit=crop&v=1461601080&auto=format&q=92&s=00b64834259065034c274deac9377d39", ["Report on the State of the Animal ","Reports from the race against climate change"]),
+
+  new User("Mark Johnson","Santa Monica, CA", "mark.john30@gmail.com","https://ksr-ugc.imgix.net/assets/015/665/532/e6ecaadfa1906c6cec80230e07e2d4bb_original.jpg?w=220&h=220&fit=crop&v=1488236452&auto=format&q=92&s=32616461312333052efb1bf22868e6a2", ["Superscreen"]),
+
+  new User("David Varela", "Miami,Florida", "david6@yahoo.com","https://ksr-ugc.imgix.net/assets/015/978/596/342b260b7e7d514119b50adede1879dc_original.jpg?w=160&h=160&fit=crop&v=1490291866&auto=format&q=92&s=fad235e30fc8fa2b81c91c50e59f2325", ["ZeTime-World's first smartwatch with touchscreen"]),
+
+  new User("Elretron", "Soquel, CA","elretron.com","https://ksr-ugc.imgix.net/assets/015/963/879/b023cf0479d889b0648539ee18c22d33_original.jpg?w=220&h=220&fit=crop&v=1490200897&auto=format&q=92&s=5332b91b7763169952413352cae43d31", ["Penna-Retro Bluetooth KeyBoard"]),
+
+  new User("Jesper Vestergaard", "Copenhagen, Denmark", "jespervegs.dk", "https://ksr-ugc.imgix.net/assets/015/845/421/9ad8d949313d0b833c42995d6789fb4c_original.jpg?w=160&h=160&fit=crop&v=1489407668&auto=format&q=92&s=920913bbf82a64aecc040286fb66e468", ["Super Famicon- a visual compendium"])
+
+]
