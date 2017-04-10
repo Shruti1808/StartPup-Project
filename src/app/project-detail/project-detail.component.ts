b00961ca0b46eb1d8a7657097e5a581680f6a40c
Alexandra Holcombe and Elise St Hilaire <alexandra+elise@>
2017-04-10 13:39:42 -0700

    Start project-detail

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
+import { Need } from '../need.model';
+import { Contact } from '../contact.model';
+import { SocialMedia } from '../social-media.model';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
-  styleUrls: ['./project-detail.component.css']
+  styleUrls: ['./project-detail.component.css'],
+  providers: [UserService, ProjectService]
 })
 export class ProjectDetailComponent implements OnInit {
+  public projectId: number;
+  public projectToDisplay: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private userService: UserService,
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.projectId = urlParameters['id'];
+    });
+    this.projectToDisplay = this.projectService.getProjectById(this.projectId).subscribe(dataLastEmittedFromObserver => {
+      this.projectToDisplay = dataLastEmittedFromObserver;
+
+      console.log(this.projectToDisplay);
+    })
   }
 
 }
