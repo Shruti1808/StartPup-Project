8c378ef687ae3808808e04a681288b0606bb9f25
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-12 15:09:17 -0700

    Resolve merge conficts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,11 +30,8 @@ import { UserProjectComponent } from './user-project/user-project.component';
 import { TwitterTimelineComponent } from './twitter-timeline/twitter-timeline.component';
 import { NeedComponent } from './need/need.component';
 import { EditProjectComponent } from './edit-project/edit-project.component';
-<<<<<<< HEAD
 import { CompleteProfileComponent } from './complete-profile/complete-profile.component';
-=======
 import { EditNeedComponent } from './edit-need/edit-need.component';
->>>>>>> 054ff40f0132a8a4c161098b0a3e70c3415442ad
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -64,13 +61,10 @@ export const firebaseConfig = {
     TwitterTimelineComponent,
     NeedComponent,
     EditProjectComponent,
-<<<<<<< HEAD
-    CompleteProfileComponent
-=======
+    CompleteProfileComponent,
     EditNeedComponent
->>>>>>> 054ff40f0132a8a4c161098b0a3e70c3415442ad
   ],
-  
+
   imports: [
     BrowserModule,
     FormsModule,
