ea22094ecf6f598e59e18f557a4a165416d62264
Elise St Hilaire and Renee Mei <elise+renee@>
2017-04-13 10:30:46 -0700

    fix twitter api

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -17,6 +17,7 @@ export class SideBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
   getType(socialMedia){
@@ -34,12 +35,14 @@ export class SideBarComponent implements OnInit {
   }
 
   getTwitterHandle(socialMedia) {
-    console.log(socialMedia);
-    for(let media of socialMedia) {
-      if(media.mediaType == "Twitter") {
-      return media.mediaAccount;
+    setTimeout(()=> {
+      console.log(socialMedia)
+      for(let media of socialMedia) {
+        if(media.mediaType == "Twitter") {
+          return media.mediaAccount;
+        }
       }
-    }
+    }, 100);
   }
 
 }
