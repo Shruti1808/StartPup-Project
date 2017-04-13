45dbcdfc6c9fd76723507e6a49b4b8674d16b81d
Elise St Hilaire and Renee Mei <elise+renee@>
2017-04-13 15:12:29 -0700

    flat html

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -26,7 +26,10 @@ export class ProjectListComponent implements OnInit {
     var limit = 150;
     var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
     var reg = new RegExp('(?=[' + trimmable + '])');
-    var words = str.split(reg);
+    var words = str.replace(/<style([\s\S]*?)<\/style>/gi, ' ')
+    .replace(/<script([\s\S]*?)<\/script>/gi, ' ')
+    .replace(/(<(?:.|\n)*?>)/gm, ' ')
+    .replace(/\s+/gm, ' ').split(reg);
     var count = 0;
     return words.filter(function(word) {
       count += word.length;
